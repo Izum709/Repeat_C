a3dd3807d2011d5fba4932de924c601904136eb8
Alexandr Vinogradov <izum709@.com>
2023-08-31 19:47:57 +0300

    Задачка с определением веса гири

---
+++ Lesson_1/Task_7/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задачка с определением веса гири
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+int maxim = a;
+if (a > maxim) maxim = a;
+if (b > maxim) maxim = b;
+if (c > maxim) maxim = c;
+if (d > maxim) maxim = d;
+if (e > maxim) maxim = e;
+Console.Write("max = ");
+Console.WriteLine(maxim);
