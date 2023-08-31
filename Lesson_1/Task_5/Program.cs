8df3dbe98e1ca34b75828ab1b1ec4e6632c06954
Alexandr Vinogradov <izum709@.com>
2023-08-31 17:35:27 +0300

    add new Random and double

---
+++ Lesson_1/Task_5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Генератор случайных чисел и double
+double a = new Random().Next(1, 10);
+double b = new Random().Next(1, 10);
+double c = 0;
+if (a > b)
+{
+    c = a / b;
+}
+if (a < b)
+{
+    c = b / a;
+}
+Console.WriteLine(c);
+Console.WriteLine(a);
+Console.WriteLine(b);
