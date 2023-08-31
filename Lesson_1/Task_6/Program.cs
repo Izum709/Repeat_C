157ca48c1f29ab60ce4fdbf92b969d074b99f654
Alexandr Vinogradov <izum709@.com>
2023-08-31 19:38:37 +0300

    add hello user name with 'if, else'

---
+++ Lesson_1/Task_6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Приветствие любимчиков через конструкцию ветвления 
+// Функция .ToLower - Перевод вводимых символов в нижний регистр
+
+Console.Write("Введите имя пользователя -> ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же Маша");
+}
+else
+{
+    Console.Write(" Привет, ");
+    Console.Write(username);
+    Console.WriteLine(" я думал это Маша");
+}
