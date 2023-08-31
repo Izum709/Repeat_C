6b959936139a5d5a97b4a72de563a1477c8aa2a1
Alexandr Vinogradov <izum709@.com>
2023-08-31 16:54:01 +0300

    add Task_2 Hi user inLine

---
+++ Lesson_1/Task_2/Program.cs
@@ -0,0 +1,5 @@
+﻿// используем команнды ввода и вывода на 1-й строке без оканчания Line
+Console.Write("add your name -> ");
+string username = Console.ReadLine();
+Console.Write("Hi ");
+Console.Write(username);
