979aa638f04c0dbfd22bb50ba65de0363e479507
Alexandr Vinogradov <izum709@.com>
2023-09-02 12:05:26 +0300

    add task_1 function, start

---
+++ Lesson_2/Task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Функции/Методы
+Console.WriteLine("Hello, Function");
+double Function(double x)
+{
+    double result = x * x; 
+    return result;
+}
+Console.Write("add number N ->  ");
+double n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Function(n));
