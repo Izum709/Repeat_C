f4fe4b499dd9a4daa5010093fc4b76acec9cd219
Alexandr Vinogradov <izum709@.com>
2023-09-02 14:24:02 +0300

    add Task_3 array, find number, break

---
+++ Lesson_2/Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Массивы. Поиск числа в массиве и вывод индекса числа
+Console.WriteLine("Hello, Array");
+int[] array = {12, 42, 36, 42, 56, 26, 4, 78, 39};
+// int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < array.Length)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        // break; в случае если искомых чисел в массиве больше чем 1, оператор break прерывает работу
+        // программы после первого найденного числа
+    }
+    index++;
+}
+
+
